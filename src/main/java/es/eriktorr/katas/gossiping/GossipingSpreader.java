d41af86a8991e78a5178d7a96580654f603d970c
Erik Torres <erik.torres@codurance.com>
2018-09-18 21:14:40 +0200

    failing acceptance test

---
+++ src/main/java/es/eriktorr/katas/gossiping/GossipingSpreader.java
@@ -0,0 +1,15 @@
+package es.eriktorr.katas.gossiping;
+
+import java.util.List;
+import java.util.Map;
+
+class GossipingSpreader {
+
+    GossipingSpreader(Map<String, List<Integer>> routes) {
+    }
+
+    String stopsNeededToSpreadAllTheGossips() {
+        return null;
+    }
+
+}
