9c9a2a181ed88878652d34c46eddac16e2110ad9
Erik Torres <erik.torres@codurance.com>
2018-09-19 09:32:51 +0200

    make a better use of the domain language

--- src/main/java/es/eriktorr/katas/gossiping/GossipingSpreader.java
+++ src/main/java/es/eriktorr/katas/gossiping/GossipingSpreader.java
@@ -1,22 +1,33 @@
 package es.eriktorr.katas.gossiping;
 
+import lombok.val;
+
 import java.util.List;
-import java.util.stream.Collectors;
+import java.util.stream.IntStream;
 
 class GossipingSpreader {
 
     private static final int MINUTES_MAXIMUM = 480;
 
-    private final List<DailyBusRoute> dailyRoutes;
+    private final List<BusRoute> routes;
 
     GossipingSpreader(List<BusRoute> routes) {
-        this.dailyRoutes = routes.stream()
-                .map(DailyBusRoute::new)
-                .collect(Collectors.toList());
+        this.routes = routes;
     }
 
     String stopsNeededToSpreadAllTheGossips() {
-        return null;
+        val stops = IntStream.rangeClosed(0, MINUTES_MAXIMUM).sorted()
+                .filter(this::areAllGossipsSpreadAt)
+                .mapToObj(Integer::toString)
+                .findFirst();
+        return stops.orElse("never");
+    }
+
+    private boolean areAllGossipsSpreadAt(int minute) {
+
+        // TODO
+
+        return false;
     }
 
 }
--- src/test/java/es/eriktorr/katas/gossiping/GossipingSpreaderTest.java
+++ src/test/java/es/eriktorr/katas/gossiping/GossipingSpreaderTest.java
@@ -13,9 +13,9 @@ public class GossipingSpreaderTest {
     find_the_number_of_stops_it_takes_to_have_all_drivers_on_board_with_the_latest_gossips() {
         val gossipingSpreader = new GossipingSpreader(
                 Arrays.asList(
-                        new BusRoute("Driver 1", Arrays.asList(3, 1, 2, 3)),
-                        new BusRoute("Driver 2", Arrays.asList(3, 2, 3, 1)),
-                        new BusRoute("Driver 3", Arrays.asList(4, 2, 3, 4, 5))
+                        new BusRoute("Dave", Arrays.asList(3, 1, 2, 3)),
+                        new BusRoute("Liz", Arrays.asList(3, 2, 3, 1)),
+                        new BusRoute("John", Arrays.asList(4, 2, 3, 4, 5))
                 )
         );
         assertThat(gossipingSpreader.stopsNeededToSpreadAllTheGossips()).isEqualTo("5");
@@ -25,8 +25,8 @@ public class GossipingSpreaderTest {
     return_never_when_at_least_one_driver_does_not_have_all_the_gossips_by_the_end_of_the_day() {
         val gossipingSpreader = new GossipingSpreader(
                 Arrays.asList(
-                        new BusRoute("Driver 1", Arrays.asList(2, 1, 2)),
-                        new BusRoute("Driver 2", Arrays.asList(5, 2, 8))
+                        new BusRoute("Dave", Arrays.asList(2, 1, 2)),
+                        new BusRoute("Liz", Arrays.asList(5, 2, 8))
                 )
         );
         assertThat(gossipingSpreader.stopsNeededToSpreadAllTheGossips()).isEqualTo("never");
